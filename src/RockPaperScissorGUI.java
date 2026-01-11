242a7b38bcb934041027bc5b6c84cf0fac831827
Viiuska <87257685+Viiuska@users.noreply.github.com>
2026-01-11 13:43:55 +0200

    Rock, Paper, and Scissor buttons added

--- src/RockPaperScissorGUI.java
+++ src/RockPaperScissorGUI.java
@@ -3,6 +3,8 @@ import java.awt.*;
 
 //User interface
 public class RockPaperScissorGUI extends JFrame {
+    JButton rockButton, paperButton, scissorButton;
+
     public RockPaperScissorGUI(){
         super("Rock Paper Scissor");
 
@@ -45,5 +47,25 @@ public class RockPaperScissorGUI extends JFrame {
         playerScoreLabel.setHorizontalAlignment(SwingConstants.CENTER);
 
         add(playerScoreLabel);
+
+        //Player Buttons
+        rockButton = new JButton("Rock");
+        rockButton.setBounds(40,387,105,81);
+        rockButton.setFont(new Font("Dialog", Font.PLAIN, 18));
+
+        add(rockButton);
+
+        paperButton = new JButton("Paper");
+        paperButton.setBounds(165,387,105,81);
+        paperButton.setFont(new Font("Dialog", Font.PLAIN, 18));
+
+        add(paperButton);
+
+        scissorButton = new JButton("Scissor");
+        scissorButton.setBounds(290,387,105,81);
+        scissorButton.setFont(new Font("Dialog", Font.PLAIN, 18));
+
+        add(scissorButton);
+
     }
 }
