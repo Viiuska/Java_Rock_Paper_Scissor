c42320a44da4806de64557c9583b559b3f28198d
Viiuska <87257685+Viiuska@users.noreply.github.com>
2026-01-11 13:28:46 +0200

    Creating the GUI template

--- src/RockPaperScissorGUI.java
+++ src/RockPaperScissorGUI.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
+import java.awt.*;
 
+//User interface
 public class RockPaperScissorGUI extends JFrame {
     public RockPaperScissorGUI(){
         super("Rock Paper Scissor");
@@ -8,7 +10,24 @@ public class RockPaperScissorGUI extends JFrame {
 
         setLayout(null);
 
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
         setLocationRelativeTo(null);
 
+        addGuiComponents();
+
+    }
+
+    //Text elements
+    private  void addGuiComponents(){
+        JLabel computerScoreLabel = new JLabel("Computer: 0");
+
+        computerScoreLabel.setBounds(0,43,450,30);
+
+        computerScoreLabel.setFont(new Font("Dialog", Font.BOLD, 26));
+
+        computerScoreLabel.setHorizontalAlignment(SwingConstants.CENTER);
+
+        add(computerScoreLabel);
     }
 }
