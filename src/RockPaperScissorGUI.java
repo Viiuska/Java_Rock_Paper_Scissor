1484cd3c2cd39d0e9e5078d3df8144dea29eb868
Viiuska <87257685+Viiuska@users.noreply.github.com>
2026-01-11 13:19:26 +0200

    Init commit

---
+++ src/RockPaperScissorGUI.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class RockPaperScissorGUI extends JFrame {
+    public RockPaperScissorGUI(){
+        super("Rock Paper Scissor");
+
+        setSize(450, 574);
+
+        setLayout(null);
+
+        setLocationRelativeTo(null);
+
+    }
+}
